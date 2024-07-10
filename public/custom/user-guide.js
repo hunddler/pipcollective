20767123b828115cebf16cb144cd77720295dde4
hunddler <137695508+hunddler@users.noreply.github.com>
2024-07-10 19:11:21 +0500

    Update user-guide.js

--- public/custom/user-guide.js
+++ public/custom/user-guide.js
@@ -182,5 +182,14 @@ $(document).ready(function() {
             });
         });
             
-    });  
+    }); 
+    
+    $(document).ready(function(){
+        $("#myInput").on("keyup", function() {
+          var value = $(this).val().toLowerCase();
+          $("#myTable tr").filter(function() {
+            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+          });
+        });
+      });
     
